bc5e2a4a9feda13b1ca605d1517ca87d81178652
Laurent Jouanneau <dev@ljouanneau.com>
2025-04-29 15:08:10 +0200

    Fix tests configuration

--- tests/testapp/app/system/index/config.ini.php
+++ tests/testapp/app/system/index/config.ini.php
@@ -21,4 +21,16 @@ on_error_action="jelix~error:badright"
 
 [acl2]
 driver=db
+hiddenRights=
+hideRights=off
+authAdapterClass=jAcl2JAuthAdapter
 
+[webassets_common]
+master_admin.css[]="$jelix/design/master_admin.css"
+jacl2_admin.require = jquery_ui
+jacl2_admin.css[]="$jelix/design/jacl2.css"
+jacl2_admin.js[]="$jelix/js/jacl2db_admin.js"
+
+jauthdb_admin.require = jquery_ui
+;jauthdb_admin.css[]="$jelix/design/jauthdb_admin.css"
+jauthdb_admin.js[]="$jelix/js/authdb_admin.js"
